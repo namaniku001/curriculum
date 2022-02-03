4a0cd925c9710893af06a1a03c3aa5877d9796bd
kenta.shinnishi <null5011@gmail.com>
2022-02-04 08:15:41 +0900

    2章 チェックテスト

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,17 +1,19 @@
 package check;
+
 import constants.Constants;
 
 public class Check {
-	private static String firstName="新西";
-    private static String lastName="健太";
+	private static String firstName = "新西";
+	private static String lastName = "健太";
+
 	private static void printName() {
-		System.out.println("printNameメソッド → "+firstName + lastName);
+		System.out.println("printNameメソッド → " + firstName + lastName);
 	}
 
 	public static void main(String[] args) {
 		printName();
-		Pet pt = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
-		RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		Pet pt = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		pt.introduce();
 		rp.introduce();
 	}
